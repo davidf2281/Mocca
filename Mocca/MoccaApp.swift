d5c106ddbd928c3bb2cf33c99cd10427eaa82578
David Fearon <david@davidfearon.com>
2020-10-11 20:11:40 +0100

    Minor edits

--- Mocca/MoccaApp.swift
+++ Mocca/MoccaApp.swift
@@ -24,11 +24,21 @@ final class MoccaApp: App, ObservableObject {
         return manager
     }()
     
-    /// Queue used to take blocking calls to the physical camera hardware off the main thread.
+    /// Camera apps cannot conform to the Apple ideal of disregarding orientation and considering only frame bounds & size classes,
+    /// thus current device orientation needs to be consumed by various views and the capture manager.
+    /// This OrientationPublisher acts as the app's source of truth for orientation.
+    private let orientationPublisher = OrientationPublisher()
+    
+    /// Queue used to take blocking calls to the camera hardware off the main thread.
     private let sessionQueue = DispatchQueue(label: "com.mocca-app.sessionQueue")
+    
+    /// Capture-session video preview (ie, camera viewfinder).
+    private let previewUIView = PreviewUIView()
     
     /// Main view hierarchy for the app.
     private var ContentViews: some View {
+        
+        // MARK: TODO: Configuring non-UI app state in this function feels like a misuse. Consider a different approach.
         
         // Photo taker responds to messages from shutter viewmodel to kick off the photo-capture process.
         self.photoTaker = DevicePhotoTaker(captureManager: captureManager ?? nil, photoLibrary: PHPhotoLibrary.shared())
@@ -43,23 +53,17 @@ final class MoccaApp: App, ObservableObject {
             .nominal : System.runningInSimulator() ?
             .nominal : .cameraFailure
         
-        /// Capture-session video preview (ie camera viewfinder).
-        let previewUIView = PreviewUIView()
-        
         // Capture manager requires a reference to the video preview layer to convert view coords to device coords using the AVCaptureVideoPreviewLayer's point-conversion functions. Only the preview layer can do this.
         captureManager?.videoPreviewLayer = previewUIView.videoPreviewLayer
         
         sessionQueue.async {
             self.captureManager?.startCaptureSession()
             DispatchQueue.main.async {
-                previewUIView.videoPreviewLayer.session = self.captureManager?.captureSession as? AVCaptureSession
-                previewUIView.videoPreviewLayer.connection?.videoOrientation = Orientation.AVOrientation(for: Orientation.currentInterfaceOrientation())
-                previewUIView.videoPreviewLayer.videoGravity = .resizeAspect
+                self.previewUIView.videoPreviewLayer.session = self.captureManager?.captureSession as? AVCaptureSession
+                self.previewUIView.videoPreviewLayer.connection?.videoOrientation = Orientation.AVOrientation(for: Orientation.currentInterfaceOrientation())
+                self.previewUIView.videoPreviewLayer.videoGravity = .resizeAspect
             }
         }
-        
-        /// Camera apps cannot conform to the Apple ideal of disregarding orientation and considering only frame bounds & size classes, thus current device orientation needs to be consumed by various views and the capture manager. This orientationPublisher acts as the source of truth for orientation.
-        let orientationPublisher = OrientationPublisher()
         
         /// Backs widget view to show current focus/exposure point
         let widgetViewModel = WidgetViewModel(captureManager: captureManager, dockedPosition:CGPoint(x: 55, y: 55), displayCharacter:"f")
