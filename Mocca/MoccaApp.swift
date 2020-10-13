7a6ed6d68c463e250210ed8ddfb42931a5fe8114
David Fearon <david@davidfearon.com>
2020-10-13 10:17:07 +0100

    Refactor app initialization for better separation of concerns

--- Mocca/MoccaApp.swift
+++ Mocca/MoccaApp.swift
@@ -15,10 +15,23 @@ final class MoccaApp: App, ObservableObject {
 
     @Published var appState: AppState = .nominal
  
-    private var photoTaker: DevicePhotoTaker?
+    /// Photo taker responds to messages from shutter viewmodel to kick off the photo-capture process.
+    private var photoTaker: DevicePhotoTaker
     
+    /// Backs widget view to show current focus/exposure point.
+    private let widgetViewModel: WidgetViewModel
+    
+    /// Backs camera preview view, sending taps to capture manager dependency to set focus/exposure point.
+    private let previewViewModel: PreviewViewModel
+    
+    /// Representable UIViewController container for camera preview, since SwiftUI does not yet support this natively.
+    private let previewViewController: PreviewViewController
+    
+    /// Backs the shutter button, sending taps to photoTaker dependency
+    private let shutterButtonViewModel: ShutterButtonViewModel
+
     /// Capture manager is the intermediary class dealing with all communication with the device's physical camera hardware.
-    private let captureManager : DeviceCaptureManager? = {
+    private let captureManager: DeviceCaptureManager? = {
         var manager : DeviceCaptureManager?
         do { try manager = DeviceCaptureManager() } catch { manager = nil }
         return manager
@@ -35,27 +48,23 @@ final class MoccaApp: App, ObservableObject {
     /// Capture-session video preview (ie, camera viewfinder).
     private let previewUIView = PreviewUIView()
     
-    /// Main view hierarchy for the app.
-    private var ContentViews: some View {
-        
-        // MARK: TODO: Configuring non-UI app state in this function feels like a misuse. Consider a different approach.
-        
-        // Photo taker responds to messages from shutter viewmodel to kick off the photo-capture process.
-        self.photoTaker = DevicePhotoTaker(captureManager: captureManager ?? nil, photoLibrary: PHPhotoLibrary.shared())
-        
-        // We must always have a PhotoTaker
-        guard let taker = self.photoTaker else {
-            fatalError("Programmer error: photoTaker must not be nil")
-        }
+    init() {
         
         // If capture-manager setup has failed we have a hardware problem, OR we're running in the simulator.
         self.appState = captureManager != nil ?
             .nominal : System.runningInSimulator() ?
             .nominal : .cameraFailure
         
-        // Capture manager requires a reference to the video preview layer to convert view coords to device coords using the AVCaptureVideoPreviewLayer's point-conversion functions. Only the preview layer can do this.
+        // Capture manager requires a reference to the video preview layer to convert view coords to camera-device coords using
+        // AVCaptureVideoPreviewLayer's point-conversion functions. Only the preview layer can do this.
         captureManager?.videoPreviewLayer = previewUIView.videoPreviewLayer
         
+        photoTaker =             DevicePhotoTaker(captureManager: captureManager ?? nil, photoLibrary: PHPhotoLibrary.shared())
+        widgetViewModel =        WidgetViewModel(captureManager: captureManager, dockedPosition:CGPoint(x: 55, y: 55), displayCharacter:"f")
+        previewViewModel =       PreviewViewModel(captureManager: captureManager)
+        previewViewController =  PreviewViewController(previewView: previewUIView, orientationPublisher: orientationPublisher)
+        shutterButtonViewModel = ShutterButtonViewModel(photoTaker: photoTaker)
+
         sessionQueue.async {
             self.captureManager?.startCaptureSession()
             DispatchQueue.main.async {
@@ -64,19 +73,10 @@ final class MoccaApp: App, ObservableObject {
                 self.previewUIView.videoPreviewLayer.videoGravity = .resizeAspect
             }
         }
-        
-        /// Backs widget view to show current focus/exposure point
-        let widgetViewModel = WidgetViewModel(captureManager: captureManager, dockedPosition:CGPoint(x: 55, y: 55), displayCharacter:"f")
-        
-        /// Backs camera preview view, sending taps to capture manager dependency to set focus/exposure point
-        let previewViewModel = PreviewViewModel(captureManager: captureManager)
-        
-        /// Backs the shutter button, sending taps to photoTaker dependency
-        let shutterButtonViewModel = ShutterButtonViewModel(photoTaker: taker)
-        
-        /// Representable UIViewController container for camera preview, since SwiftUI does not yet support this natively
-        let previewViewController = PreviewViewController(previewView: previewUIView, orientationPublisher: orientationPublisher)
-        
+    }
+    
+    /// Main view hierarchy for the app.
+    private var ContentViews: some View {
         // Compose our main app view.
         return ContentView(
             app: self,
