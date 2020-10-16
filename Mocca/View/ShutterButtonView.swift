a29d990d4a4360fe108beb80fa494c2d07ab5249
David Fearon <david@davidfearon.com>
2020-10-16 17:01:49 +0100

    Add better state indication to ShutterButtonView

--- Mocca/View/ShutterButtonView.swift
+++ Mocca/View/ShutterButtonView.swift
@@ -17,14 +17,15 @@ struct ShutterButtonView<Model>: View where Model: ShutterButtonViewModelProtoco
     
     var body: some View {
         ZStack {
-            Circle().stroke(style: viewModel.state == .ready ? solidStroke : dashedStroke)
+            Circle().stroke(style: viewModel.state == .ready ? solidStroke : viewModel.state == .capturePending ? dashedStroke : viewModel.state == .saving ? dashedStroke : solidStroke)
                 .frame(width: 65, height: 65, alignment: .center)
                 .rotation3DEffect(
                     viewModel.state != .ready ? Angle(degrees: 360) : Angle(degrees: 0), axis: (x: 0.0, y: 0.0, z: 1.0), perspective: 0
                 )
                 .foregroundColor(.gray)
                 .animation(viewModel.state != .ready ? continuousAnimation : .easeIn(duration: 0))
-            Circle().fill(viewModel.state == .ready ? Color.red : Color.gray)
+            Circle()
+                .fill(viewModel.state == .ready ? Color.red : Color.gray)
                 .frame(width: 50, height: 50, alignment: .center)
         }.onTapGesture(count: 1, perform: {
             self.viewModel.tapped()
