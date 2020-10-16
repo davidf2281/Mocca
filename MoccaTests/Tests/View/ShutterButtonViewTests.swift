bf27c68fd0c8a194f7d9c66cb4c62fd0d82d8a6e
David Fearon <david@davidfearon.com>
2020-10-16 17:02:46 +0100

    Add more ShutterButtonView unit tests

--- MoccaTests/Tests/View/ShutterButtonViewTests.swift
+++ MoccaTests/Tests/View/ShutterButtonViewTests.swift
@@ -13,10 +13,20 @@ import ViewInspector
 
 extension ShutterButtonView: Inspectable { }
 
+private class MockShutterButtonViewModel: ShutterButtonViewModelProtocol {
+    var state: PhotoTakerState = .ready
+    private(set) var tapCalled = false
+    func tapped() {
+        self.tapCalled = true
+    }
+}
+
 final class ShutterButtonViewTests: XCTestCase {
 
-    private var viewModel = MockShutterButtonViewModel()
-    
+    private var viewModel: MockShutterButtonViewModel!
+    private let dashedStroke = StrokeStyle( lineWidth: 3, dash: [10] )
+    private let solidStroke = StrokeStyle( lineWidth: 3)
+
     override func setUp() {
         viewModel = MockShutterButtonViewModel()
     }
@@ -25,34 +35,104 @@ final class ShutterButtonViewTests: XCTestCase {
         XCTAssertEqual(viewModel.state, .ready)
     }
     
+    func testViewModelTappedCalledOnTap() throws {
+        XCTAssertFalse(viewModel.tapCalled)
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        try sut.inspect().zStack().callOnTapGesture()
+        XCTAssertTrue(viewModel.tapCalled)
+    }
+    
     func testOuterCircleColorIsGray() throws {
         let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
-        
         let value = try sut.inspect().zStack().shape(0).foregroundColor()
         XCTAssertEqual(value, Color.gray)
     }
     
-    func testOuterCircleStyleIsSolidWhenReady() throws {
+    func testOuterCircleStyleIsSolidWhenStateIsReady() throws {
         let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
-
-        let solidStroke = StrokeStyle( lineWidth: 3)
         let value = try sut.inspect().zStack().shape(0).strokeStyle()
         XCTAssertEqual(value, solidStroke)
     }
     
-    func testOuterCircleStyleIsDashedWhenCapturePending() throws {
+    func testOuterCircleStyleIsDashedWhenStateIsCapturePending() throws {
         let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
-                
-        let dashedStroke = StrokeStyle( lineWidth: 3, dash: [10] )
         viewModel.state = .capturePending
         let value = try sut.inspect().zStack().shape(0).strokeStyle()
         XCTAssertEqual(value, dashedStroke)
     }
+    
+    func testOuterCircleStyleIsDashedWhenStateIsSaving() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .saving
+        let value = try sut.inspect().zStack().shape(0).strokeStyle()
+        XCTAssertEqual(value, dashedStroke)
+    }
+    
+    func testOuterCircleStyleIsSolidWhenStateIsCaptureError() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .captureError
+        let value = try sut.inspect().zStack().shape(0).strokeStyle()
+        XCTAssertEqual(value, solidStroke)
+    }
+    
+    func testOuterCircleStyleIsSolidWhenStateIsSaveError() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .saveError
+        let value = try sut.inspect().zStack().shape(0).strokeStyle()
+        XCTAssertEqual(value, solidStroke)
+    }
+
+    func testOuterCircleFrameSize() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+
+        let value = try sut.inspect().zStack().shape(0).fixedFrame()
+        XCTAssertEqual(value.width,  65)
+        XCTAssertEqual(value.height, 65)
+    }
+    
+    func testInnerCircleFrameSize() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+
+        let value = try sut.inspect().zStack().shape(1).fixedFrame()
+        XCTAssertEqual(value.width,  50)
+        XCTAssertEqual(value.height, 50)
+    }
+
+    func testInnerCircleIsRedWhenStateIsReady() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+
+        let value = try sut.inspect().zStack().shape(1).fillShapeStyle(Color.self)
+
+        XCTAssertEqual(value, .red)
+    }
+    
+    func testInnerCircleIsGrayWhenStateIsCaptureError() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .captureError
+        let value = try sut.inspect().zStack().shape(1).fillShapeStyle(Color.self)
+        XCTAssertEqual(value, .gray)
+    }
+    
+    func testInnerCircleIsGrayWhenStateIsCapturePending() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .capturePending
+        let value = try sut.inspect().zStack().shape(1).fillShapeStyle(Color.self)
+        XCTAssertEqual(value, .gray)
+    }
+    
+    func testInnerCircleIsGrayWhenStateIsSaveError() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .saveError
+        let value = try sut.inspect().zStack().shape(1).fillShapeStyle(Color.self)
+        XCTAssertEqual(value, .gray)
+    }
+    
+    func testInnerCircleIsGrayWhenStateIsSaving() throws {
+        let sut = ShutterButtonView<MockShutterButtonViewModel>(viewModel: viewModel)
+        viewModel.state = .saving
+        let value = try sut.inspect().zStack().shape(1).fillShapeStyle(Color.self)
+        XCTAssertEqual(value, .gray)
+    }
 }
 
-class MockShutterButtonViewModel: ShutterButtonViewModelProtocol {
-    
-    var state: PhotoTakerState = .ready
-    
-    func tapped() {}
-}
+
