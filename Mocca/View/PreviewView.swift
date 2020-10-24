c325d48a9271b2c061b9519414e5144e0028df64
David Fearon <david@davidfearon.com>
2020-10-24 13:11:11 +0100

    Fix tests

--- Mocca/View/PreviewView.swift
+++ Mocca/View/PreviewView.swift
@@ -62,8 +62,8 @@ struct PreviewModifier: ViewModifier {
                     .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
                                 .onEnded { gesture in
                                     let frameSize = CGSize(width: parent.size.width - (edgeInsets.leading + edgeInsets.trailing), height: parent.size.height - (edgeInsets.top + edgeInsets.bottom))
-                                    let adjustedPosition = verticalSizeClass == .regular ? CGPoint(x: gesture.location.x - margin, y: gesture.location.y) : CGPoint(x: gesture.location.x, y: gesture.location.y - margin)
-                                    let position = ViewConversion.tapPosition(position: adjustedPosition,
+//                                    let adjustedPosition = verticalSizeClass == .regular ? CGPoint(x: gesture.location.x /*- margin*/, y: gesture.location.y) : CGPoint(x: gesture.location.x, y: gesture.location.y /*- margin*/)
+                                    let position = ViewConversion.tapPosition(position: gesture.location,
                                                                               orientation: orientationPublisher.interfaceOrientation, parentFrame: frameSize)
                                     self.widgetViewModel.position = position
                                     self.previewViewModel.tapped(position: gesture.location, frameSize:frameSize)
